540ccd6064455dbdf2bdf72722a360aa555a11bc
Alexandr Martynov <mag@unicont.com>
2024-09-03 20:03:17 +0300

    Changed detection of terminal capabilities by environment vars

--- source/platform/termdisp.cpp
+++ source/platform/termdisp.cpp
@@ -4,6 +4,10 @@
 #include <internal/termdisp.h>
 #include <internal/linuxcon.h>
 #include <internal/getenv.h>
+//
+#if !defined( __BORLANDC__ )
+#include <string_view>
+#endif
 
 namespace tvision
 {
@@ -11,10 +15,97 @@ namespace tvision
 TermCap TerminalDisplay::getCapabilities() noexcept
 {
     TermCap termcap {};
+
+    bool termXterm = false;
+
+#if !defined(TV_BARE_METAL)
+
+    // Allow env var detection if not a bare metal
+
+#if defined( __BORLANDC__ )
+    // TStringView is only for BCC
     auto colorterm = getEnv<TStringView>("COLORTERM");
     if (colorterm == "truecolor" || colorterm == "24bit")
+    {
         termcap.colors = Direct;
+    }
+#else
+    // Use normal std::string_view
+    // TERM in Debian displays "xterm-256color"
+    // COLORTERM in Debian is empty
+
+    // Changing detection of terminal capabilities by environment vars
+    auto termStr      = getEnv<std::string_view>("TERM");
+    auto colortermStr = getEnv<std::string_view>("COLORTERM");
+
+    bool term256c  = false;
+    bool term24bit = false;
+    if (!termStr.empty())
+    {
+        auto pos = termStr.find("xterm", 0);
+        if (pos==0)
+        {
+            // found at start
+            termXterm = true;
+        }
+    
+        pos = termStr.find("256color", 0);
+        if (pos!=termStr.npos)
+        {
+            term256c = true;
+        }
+
+        pos = termStr.find("truecolor", 0);
+        if (pos!=termStr.npos)
+        {
+            term24bit = true;
+        }
+
+        pos = termStr.find("24bit", 0);
+        if (pos!=termStr.npos)
+        {
+            term24bit = true;
+        }
+    }
+
+    if (!colortermStr.empty())
+    {
+        auto pos = termStr.find("256color", 0);
+        if (pos!=termStr.npos)
+        {
+            term256c = true;
+        }
+
+        pos = termStr.find("truecolor", 0);
+        if (pos!=termStr.npos)
+        {
+            term24bit = true;
+        }
+    
+        pos = termStr.find("24bit", 0);
+        if (pos!=termStr.npos)
+        {
+            term24bit = true;
+        }
+    
+    }
+
+
+    if (term256c)
+    {
+        termcap.colors = Indexed256;
+    }
+    else if (term24bit)
+    {
+        termcap.colors = Direct;
+    }
+#endif
+
     else
+
+#endif // !defined(TV_BARE_METAL)
+
+    // Common term capabilities detection
     {
         int colors = getColorCount();
         if (colors >= 256*256*256)
@@ -27,12 +118,15 @@ TermCap TerminalDisplay::getCapabilities() noexcept
         {
             termcap.colors = Indexed8;
             termcap.quirks |= qfBoldIsBright;
+#if !defined(TV_BARE_METAL)
 #ifdef __linux__
             if (io.isLinuxConsole())
                 termcap.quirks |= qfBlinkIsBright | qfNoItalic | qfNoUnderline;
             else
 #endif // __linux__
-            if (getEnv<TStringView>("TERM") == "xterm")
+#endif // !defined(TV_BARE_METAL)
+            //if (getEnv<TStringView>("TERM") == "xterm")
+            if (termXterm)
                 // Let's assume all terminals disguising themselves as 'xterm'
                 // support at least 16 colors.
                 termcap.colors = Indexed16;
